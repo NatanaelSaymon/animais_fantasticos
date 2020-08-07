ff06183c035388b03be87af9bc386b8dfdb44ae2
Natanael Saymon <nsaymonsouza@gmail.com>
2020-08-07 12:13:23 -0300

    Done: navegação por tabs

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+/* NAVEGAÇÃO POR TAB */
+
+function initTabNav() {
+  const tabAnimal = document.querySelectorAll('.js-tabAnimal li')
+  const tabContent = document.querySelectorAll('.js-tabContent section')
+
+  if (tabAnimal.length && tabContent) {
+    tabContent[0].classList.add('active')
+
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove('active')
+      })
+      tabContent[index].classList.add('active')
+    }
+
+    tabAnimal.forEach((imageItem, index) => {
+      imageItem.addEventListener("click", () => {
+        activeTab(index)
+      })
+    })
+  }
+}
+
+initTabNav()
