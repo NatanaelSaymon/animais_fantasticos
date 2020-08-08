b27737a107fa7f78ab12e4a250e1d7e282427687
Natanael Saymon <nsaymonsouza@gmail.com>
2020-08-07 21:11:05 -0300

    Done: scroll animation inserido

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,3 +44,27 @@ function initAccordionList() {
 }
 
 initAccordionList()
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll')
+  const windowMetade = window.innerHeight * 0.6
+
+  function scroll() {
+    sections.forEach((item) => {
+      const section = item.getBoundingClientRect().top
+      const sectionVisible = (section - windowMetade) < 0
+
+      if (sectionVisible) {
+        item.classList.add('active')
+      } else {
+        item.classList.remove('active')
+      }
+    })
+  }
+
+  scroll()
+
+  window.addEventListener("scroll", scroll)
+}
+
+initScrollAnimation()
